905cd7ce2eb40bc8171bce07d8a5d51935014f7d
xiaobei <3187@qq.com>
2020-12-08 19:50:05 +0800

    第一次提交

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRouter = require('./router/user');
+var app = express();
+
+app.use('/api/user', userRouter)
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.listen(3000, () => {
+    console.log('Vue_project Runing...')
+})
